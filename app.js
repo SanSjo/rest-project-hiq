af41336b958d0c5b2732b6f284fb6d546bf3b9fd
SanSjo <susanna.sjoberg@gmail.com>
2021-09-20 14:33:47 +0200

    add POST request and mongodb connection

--- app.js
+++ app.js
@@ -1,14 +1,47 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.static("public"));
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/restHiQDB');
+
+const greetingSchema = {
+    content: String
+}
+
+const Greeting = mongoose.model('Greetings', greetingSchema);
+
 app.get("/greeting", (req,res) => {
-    const content = {
-        id: "1",
-        content: "Hello World"
-    }
-    res.send(content);
+    Greeting.find((err, foundGreetings) => {
+        if(!err){
+            res.send(foundGreetings)
+            console.log(foundGreetings)
+        } else {
+            res.send(err)
+        }
+    })
   
+})
+
+app.post("/greeting", (req, res) => {
+    const createGreeting = new Greeting({
+        content: req.body.content
+    })
+    createGreeting.save((err) => {
+        if(err){
+            res.send(err);
+        } else {
+            res.send("Greeting added")
+        }
+    });
+    
+    
 })
 
 app.listen(3100, () => {
